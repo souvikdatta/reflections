7dacf041cab6dc7cd0369159ca03d27d57a2933b
Souvik Datta <sd.souvikdatta@gmail.com>
2019-04-02 17:24:56 +0530

    Added sum function to support float addtion

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+float sum(float a, float b);
+
+int main(int argc, char *argv[])
+{
+        sum(1,2);
+        return 0;
+}
+        
+float sum(float a, float b);
+{       
+        return (float) (a+b);
+}       
+~ 
